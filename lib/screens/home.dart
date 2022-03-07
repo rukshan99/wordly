dfecb80c5f91eb1b59b54167bd238325e78693e4
MSI PC <navodinith@gmail.com>
2022-03-07 20:47:33 +0530

    home screen

---
+++ lib/screens/home.dart
@@ -0,0 +1,50 @@
+
+import 'package:flutter/material.dart';
+import 'package:wordly/utils/color.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  navigateQuiz() async {
+    Navigator.pushReplacementNamed(context, "quiz");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: purpleColors,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            child: Center(
+              child: Image.asset('assets/img/Logo.png'),
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              padding: EdgeInsets.fromLTRB(100, 15, 100, 15),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(100.0)), // foreground
+            ),
+            onPressed: () {
+              navigateQuiz();
+            },
+            child: Text(
+              'Start Quiz',
+              style: TextStyle(fontSize: 18),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
